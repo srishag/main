22ed23747b9c71621a64af1b942e73895575d390
tweijie <tweijie216@gmail.com>
2017-10-14 14:50:31 +0800

    add command implementation

--- src/main/java/seedu/address/logic/commands/ListTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTagsCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +12,7 @@ import seedu.address.model.tag.Tag;
 /**
  * List all tags in the addressbook to the user
  */
-public class ListTagsCommand extends Command{
+public class ListTagsCommand extends Command {
     public static final String COMMAND_WORD = "listtags";
 
     public static final String MESSAGE_SUCCESS = "You have the following tags: ";
@@ -18,23 +20,30 @@ public class ListTagsCommand extends Command{
     private static List<Tag> listOfAllTags;
     private static String namesInListOfAllTags;
 
-    public ListTagsCommand(){
-        listOfAllTags = new ArrayList<Tag>();
-        namesInListOfAllTags = "";
-
-        for (ReadOnlyPerson p: model.getFilteredPersonList()) {
-            for (Tag tag : p.getTags()){
-                if (!listOfAllTags.contains(tag)){
-                    listOfAllTags.add(tag);
-                    namesInListOfAllTags = namesInListOfAllTags + " " + tag.getTagName();
-                }
-            }
-        }
-
-    }
 
     @Override
     public CommandResult execute() throws CommandException {
+
+        listOfAllTags = new ArrayList<Tag>();
+        namesInListOfAllTags = "";
+      /*
+        for (Tag tag : model.getAddressBook().getTagList()) {
+            if (!listOfAllTags.contains(tag)) {
+                listOfAllTags.add(tag);
+                namesInListOfAllTags = namesInListOfAllTags + " " + tag.getTagName();
+            }
+        }
+        */
+
+      for (ReadOnlyPerson p : model.getAddressBook().getPersonList()){
+          for (Tag tag : p.getTags()){
+              if (!listOfAllTags.contains(tag)) {
+                  listOfAllTags.add(tag);
+                  namesInListOfAllTags = namesInListOfAllTags + " " + tag.getTagName();
+              }
+          }
+      }
+
         return new CommandResult(MESSAGE_SUCCESS + namesInListOfAllTags);
     }
 }
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,6 +17,7 @@ import seedu.address.logic.commands.FindPersonsWithTagsCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListTagsCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
@@ -74,6 +75,9 @@ public class AddressBookParser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case ListTagsCommand.COMMAND_WORD:
+            return new ListTagsCommand();
 
         case HistoryCommand.COMMAND_WORD:
             return new HistoryCommand();
