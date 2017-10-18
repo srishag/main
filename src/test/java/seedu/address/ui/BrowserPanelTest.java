82758c8c0f29f111a5f5bb6e5040e1378160425a
tweijie <tweijie216@gmail.com>
2017-10-18 23:54:21 +0800

    Revert "fixing tests"
    This reverts commit 71fd2dd23fce83a74379a20475b0fb74e31a6f75.

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
+import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_PREFIX;
+import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_SUFFIX;
 import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
 
 import java.net.URL;
@@ -40,7 +42,8 @@ public class BrowserPanelTest extends GuiUnitTest {
 
         // associated web page of a person
         postNow(selectionChangedEventStub);
-        URL expectedPersonUrl = new URL(ALICE.getFacebookAddress().value);
+        URL expectedPersonUrl = new URL(GOOGLE_SEARCH_URL_PREFIX
+                + ALICE.getName().fullName.replaceAll(" ", "+") + GOOGLE_SEARCH_URL_SUFFIX);
 
         waitUntilBrowserLoaded(browserPanelHandle);
         assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
