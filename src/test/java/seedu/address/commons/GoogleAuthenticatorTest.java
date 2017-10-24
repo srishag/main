3909dec4e4bf6158b29aeacd12876ada7cc65f2d
DESKTOP-K541CJB\phuaj <phuajunjie@hotmail.com>
2017-10-24 20:19:00 +0800

    Travis CI fix 3

--- src/test/java/seedu/address/commons/GoogleAuthenticatorTest.java
+++ src/test/java/seedu/address/commons/GoogleAuthenticatorTest.java
@@ -1,35 +1,32 @@
 package seedu.address.commons;
 
-
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class GoogleAuthenticatorTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    GoogleAuthenticator authenticator = new GoogleAuthenticator();
+    private GoogleAuthenticator authenticator = new GoogleAuthenticator();
 
     /**
      * Checks if Login url generated is in the valid format
      */
     @Test
     public void execute_obtain_loginUrl() {
-        String url =authenticator.getAuthorizationUrl();
         assertTrue(authenticator.getAuthorizationUrl().contains(
-                "https://accounts.google.com/o/oauth2/auth?client_id=650819214900"));
+                "https://accounts.google.com/o/oauth2/auth?client_id"));
     }
 
     /**
      * Checks if Login is authenticated. In this case it is not and null exception is thrown.
      */
     @Test
-    public void execute_get_invalid_token() throws Exception {
+    public void execute_get_invalidToken() throws Exception {
         thrown.expect(NullPointerException.class);
         authenticator.getToken();
     }
@@ -38,7 +35,7 @@ public class GoogleAuthenticatorTest {
      * Checks if Login is authenticated. In this case it is not and null exception is thrown.
      */
     @Test
-    public void execute_get_invalid_googlePersonList() throws Exception {
+    public void execute_get_invalidGooglePersonList() throws Exception {
         thrown.expect(NullPointerException.class);
         authenticator.getConnections(null);
     }
