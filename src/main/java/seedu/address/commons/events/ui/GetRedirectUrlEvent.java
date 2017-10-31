e120453f1db5c3030cdec99f491b49fdf5fbfa0b
srishag <srishti.23997@gmail.com>
2017-10-31 12:17:10 +0800

    travis checks

--- src/main/java/seedu/address/commons/events/ui/GetRedirectUrlEvent.java
+++ src/main/java/seedu/address/commons/events/ui/GetRedirectUrlEvent.java
@@ -2,30 +2,32 @@ package seedu.address.commons.events.ui;
 
 import seedu.address.commons.events.BaseEvent;
 
-
 /**
  * Represents a selection change in the Person List Panel
  */
 public class GetRedirectUrlEvent extends BaseEvent {
 
+        private String reDirectUrl;
 
-    private String reDirectUrl;
+        public GetRedirectUrlEvent() {
+        }
 
-    public GetRedirectUrlEvent() {
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+
+        //Setter of redirect URL
+        public void setRedirectUrl(String reDirectUrl) {
+            this.reDirectUrl = reDirectUrl;
+        }
+
+        //Getter
+        public String getReDirectUrl() {
+            return reDirectUrl;
+        }
     }
 
-    @Override
-    public String toString() {
-        return this.getClass().getSimpleName();
-    }
+}
 
-    //Setter of redirect URL
-    public void setRedirectUrl(String reDirectUrl) {
-        this.reDirectUrl = reDirectUrl;
-    }
-
-    //Getter
-    public String getReDirectUrl() {
-        return reDirectUrl;
-    }
 }
