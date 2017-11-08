6fe66c5c303840ba9e896859650257b36ae6f2bd
srishag <srishti.23997@gmail.com>
2017-11-08 17:36:23 +0800

    adding task list panel

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -16,6 +16,11 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.task.Deadline;
+import seedu.address.model.task.Desc;
+import seedu.address.model.task.Header;
+import seedu.address.model.task.Task;
+import seedu.address.model.task.exceptions.DuplicateTaskException;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
@@ -23,32 +28,44 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         try {
-            return new Person[] {
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"), //head
-                    new Address("Blk 30 Geylang Street 29, #06-40"), new Birthday("03041987"),
+            return new Person[]{
+                    new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"), //head
+                            new Address("Blk 30 Geylang Street 29, #06-40"), new Birthday("03041987"),
                             new FacebookAddress(""), getTagSet("friends"), new GoogleId("not GoogleContact")),
 
-                new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Birthday("28031990"),
-                        new FacebookAddress("BerniceYu"), getTagSet("colleagues", "friends"),
-                        new GoogleId("not GoogleContact")),
+                    new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                            new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Birthday("28031990"),
+                            new FacebookAddress("BerniceYu"), getTagSet("colleagues", "friends"),
+                            new GoogleId("not GoogleContact")),
 
-                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Birthday("18111993"),
-                    new FacebookAddress("CharlotteO"), getTagSet("neighbours"),
-                        new GoogleId("not GoogleContact")),
+                    new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                            new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Birthday("18111993"),
+                            new FacebookAddress("CharlotteO"), getTagSet("neighbours"),
+                            new GoogleId("not GoogleContact")),
 
-                new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Birthday("15072010"),
-                    new FacebookAddress("davidli"), getTagSet("family"), new GoogleId("not GoogleContact")),
+                    new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                            new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Birthday("15072010"),
+                            new FacebookAddress("davidli"), getTagSet("family"), new GoogleId("not GoogleContact")),
 
-                new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                    new Address("Blk 47 Tampines Street 20, #17-35"), new Birthday("17121994"),
-                    new FacebookAddress("ii"), getTagSet("classmates"), new GoogleId("not GoogleContact")),
+                    new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                            new Address("Blk 47 Tampines Street 20, #17-35"), new Birthday("17121994"),
+                            new FacebookAddress("ii"), getTagSet("classmates"), new GoogleId("not GoogleContact")),
 
-                new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Address("Blk 45 Aljunied Street 85, #11-31"), new Birthday("03081970"),
-                    new FacebookAddress("roy"), getTagSet("colleagues"), new GoogleId("not GoogleContact"))
+                    new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                            new Address("Blk 45 Aljunied Street 85, #11-31"), new Birthday("03081970"),
+                            new FacebookAddress("roy"), getTagSet("colleagues"), new GoogleId("not GoogleContact"))
+            };
+        } catch (IllegalValueException e) {
+            throw new AssertionError("sample data cannot be invalid", e);
+        }
+    }
+
+    public static Task[] getSampleTasks() {
+        try {
+            return new Task[]{
+                    new Task(new Header("Homework"), new Desc("Page 6 to 9"), new Deadline("27/11/2017")),
+                    new Task(new Header("Assignment"), new Desc("Tutorial homework"), new Deadline("05/12/2017")),
+                    new Task(new Header("Project"), new Desc("Finish Verilog code"), new Deadline("13/12/2017"))
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
@@ -61,8 +78,13 @@ public class SampleDataUtil {
             for (Person samplePerson : getSamplePersons()) {
                 sampleAb.addPerson(samplePerson);
             }
+
+            for (Task sampleTask : getSampleTasks()) {
+                sampleAb.addTask(sampleTask);
+            }
+
             return sampleAb;
-        } catch (DuplicatePersonException e) {
+        } catch (DuplicatePersonException | DuplicateTaskException e) {
             throw new AssertionError("sample data cannot contain duplicate persons", e);
         }
     }
