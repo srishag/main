1096bb7ff8192d8c68acde579e89c66d12e2472d
tweijie <tweijie216@gmail.com>
2017-11-09 01:35:06 +0800

    20 sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -24,6 +24,7 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         try {
             return new Person[] {
+
                 new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"), //head
                     new Address("Blk 30 Geylang Street 29, #06-40"), new Birthday("03041987"),
                             new FacebookAddress(""), getTagSet("friends"), new GoogleId("not GoogleContact")),
@@ -45,6 +46,71 @@ public class SampleDataUtil {
                 new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                     new Address("Blk 47 Tampines Street 20, #17-35"), new Birthday("17121994"),
                     new FacebookAddress("ii"), getTagSet("classmates"), new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Kelvin Naomi"), new Phone("96847231"), new Email("kelvin@example.com"),
+                        new Address("Blk 13 Naomi Street 20, #17-35"), new Birthday("12121994"),
+                        new FacebookAddress("kelvin"), getTagSet("classmates"), new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Lovell Josephine"), new Phone("96847232"), new Email("Lovell@example.com"),
+                        new Address("Blk 23 Josephine Street 20, #17-35"), new Birthday("12101967"),
+                        new FacebookAddress("Lovell"), getTagSet("schoolmates"), new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Shevon Ocean"), new Phone("96847233"), new Email("Ocean@example.com"),
+                       new Address("Blk 33 Shevon Street 20, #17-35"), new Birthday("12111994"),
+                       new FacebookAddress("Shevon"), getTagSet("friends"), new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Reagan Deforest"), new Phone("96847243"), new Email("Reagen@example.com"),
+                        new Address("Blk 322 Reagen Street 20, #17-35"), new Birthday("12111974"),
+                        new FacebookAddress("kelvin"), getTagSet("friends"), new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Roxy Glenda"), new Phone("96842233"), new Email("Glenda@example.com"),
+                        new Address("Blk 323 Roxy Street 20, #17-35"), new Birthday("12111994"),
+                        new FacebookAddress("Glenda"), getTagSet("friends", "colleagues"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Jamey Lillia"), new Phone("95684343"), new Email("Lillia@example.com"),
+                        new Address("Blk 312 Lillia Street 22, #12-34"), new Birthday("23101994"),
+                        new FacebookAddress("Lillia"), getTagSet("friends"), new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Kaylee Bryanna"), new Phone("96842233"), new Email("Bryanna@example.com"),
+                        new Address("Blk 323 Bryanna Street 20, #17-35"), new Birthday("12031994"),
+                        new FacebookAddress("Kaylee"), getTagSet("friends", "colleagues"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Gervase Steve"), new Phone("95748321"), new Email("Gervase@example.com"),
+                        new Address("Blk 32 Steve Street 22, #2-34"), new Birthday("12102994"),
+                        new FacebookAddress("GervaseSteve"), getTagSet("friends"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Brendon Bram"), new Phone("96584743"), new Email("Brendon@example.com"),
+                        new Address("Blk 323 Brendon Street 9, #9-35"), new Birthday("12031994"),
+                        new FacebookAddress("Brendon"), getTagSet("friends", "classmates", "neighbours"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Dane Edric"), new Phone("96823133"), new Email("Edric@example.com"),
+                        new Address("Blk 968 Edric Street 54, #13-22"), new Birthday("12111992"),
+                        new FacebookAddress("Edric"), getTagSet("friends", "colleagues"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Tawnee Merilyn"), new Phone("95684343"), new Email("Tawnee@example.com"),
+                        new Address("Blk 685 Tawnee Street 22, #14-34"), new Birthday("09091993"),
+                        new FacebookAddress("Tawnee"), getTagSet("friends"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Ellen Daryl"), new Phone("96842233"), new Email("Ellen@example.com"),
+                        new Address("Blk 657 Ellen Street 20, #6-52"), new Birthday("12071994"),
+                        new FacebookAddress("Ellen"), getTagSet("friends", "colleagues"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Sonnie Kate"), new Phone("95742321"), new Email("Sonnie@example.com"),
+                        new Address("Blk 32 Sonnie Street 22, #2-34"), new Birthday("12111973"),
+                        new FacebookAddress("GervaseSteve"), getTagSet("friends"),
+                        new GoogleId("not GoogleContact")),
+
+                new Person(new Name("Alanis Otis"), new Phone("96584743"), new Email("Otis@example.com"),
+                        new Address("Blk 323 Alanis Street 9, #9-35"), new Birthday("10101956"),
+                        new FacebookAddress("Otis"), getTagSet("friends", "classmates", "neighbours"),
+                        new GoogleId("not GoogleContact")),
 
                 new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                     new Address("Blk 45 Aljunied Street 85, #11-31"), new Birthday("03081970"),
