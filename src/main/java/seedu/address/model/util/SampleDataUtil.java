7bc299c73d26b85347552fb6a45e66d38172ac7d
tweijie <tweijie216@gmail.com>
2017-11-09 01:40:26 +0800

    v1.5 release

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -27,7 +27,7 @@ public class SampleDataUtil {
 
                 new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"), //head
                     new Address("Blk 30 Geylang Street 29, #06-40"), new Birthday("03041987"),
-                            new FacebookAddress(""), getTagSet("friends"), new GoogleId("not GoogleContact")),
+                            new FacebookAddress("Alex"), getTagSet("friends"), new GoogleId("not GoogleContact")),
 
                 new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Birthday("28031990"),
