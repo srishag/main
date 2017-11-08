1677651f1cf441a636ca5c4901bdf370db03aea5
srishag <srishti.23997@gmail.com>
2017-11-09 03:23:20 +0800

    travis abstract error

--- src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTaskCommandTest.java
@@ -13,9 +13,11 @@ import javafx.collections.ObservableList;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.exceptions.GoogleAuthException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
@@ -128,6 +130,11 @@ public class AddTaskCommandTest {
 
         @Override
         public void deleteTag(Tag tag) throws PersonNotFoundException, DuplicatePersonException {
+            fail("This method should not be called");
+        }
+
+        @Override
+        public void sendEmail(Index index, String subject, String body) throws CommandException, GoogleAuthException {
             fail("This method should not be called");
         }
 
